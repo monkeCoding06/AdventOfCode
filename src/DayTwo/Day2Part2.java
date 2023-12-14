9f154136c5056634d7213a2287e4b5193b31dcbc
Lukas <lkohlfuerst@eurofunk.com>
2023-12-14 11:37:02 +0100

    solved day 2 part 2

--- src/DayTwo/Day2Part2.java
+++ src/DayTwo/Day2Part2.java
@@ -3,15 +3,40 @@ package DayTwo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day2Part2 {
-    File inputDay2 = new File("src\\DayTwo\\advent2.txt");
-
     public void run() throws FileNotFoundException {
+        int result = getResult();
+        System.out.println("day two part two result: " + result);
+    }
+
+    public int getResult() throws FileNotFoundException {
+        int sum = 0;
+        File inputDay2 = new File("src\\DayTwo\\advent2.txt");
         Scanner scannerInput = new Scanner(inputDay2);
         while (scannerInput.hasNextLine()) {
             String data = scannerInput.nextLine();
-            System.out.println(data);
+            int biggestNumberRed = extractEachBiggestNumberPerColor(data, "red");
+            int biggestNumberBlue = extractEachBiggestNumberPerColor(data, "blue");
+            int biggestNumberGreen = extractEachBiggestNumberPerColor(data, "green");
+            int preSum = (biggestNumberRed * biggestNumberBlue * biggestNumberGreen);
+            sum += preSum;
         }
+        return sum;
+    }
+
+    protected int extractEachBiggestNumberPerColor(String data, String color) {
+        int biggestNumber = 0;
+        Pattern colorExtractNumberPattern = Pattern.compile("(\\d+) " + color);
+        Matcher colorMatcher = colorExtractNumberPattern.matcher(data);
+        while (colorMatcher.find()) {
+            int currentNumber = Integer.parseInt(colorMatcher.group(1));
+            if (currentNumber > biggestNumber) {
+                biggestNumber = currentNumber;
+            }
+        }
+        return biggestNumber;
     }
 }
