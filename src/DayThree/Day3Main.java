e2046fc0675e13a59f027047870b2fc6620f6c70
Lukas <lkohlfuerst@eurofunk.com>
2023-12-19 15:50:15 +0100

    implemented day 3 part 1

---
+++ src/DayThree/Day3Main.java
@@ -0,0 +1,11 @@
+package DayThree;
+
+
+import java.io.IOException;
+
+public class Day3Main {
+    public void run() throws IOException {
+    Day3Part1 day3Part1 = new Day3Part1();
+    day3Part1.run();
+    }
+}
--- src/DayThree/Day3Part1.java
+++ src/DayThree/Day3Part1.java
@@ -1,24 +1,81 @@
 package DayThree;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day3Part1 {
 
-    protected int getResult() throws FileNotFoundException {
-        int sum = 0;
-        File inputDay2 = new File("src\\DayTwo\\advent3.txt");
-        Scanner scannerInput = new Scanner(inputDay2);
-        while (scannerInput.hasNextLine()) {
-            String data = scannerInput.nextLine();
-            System.out.println(data);
+    private static final List<String> lines = new LinkedList<>();
+
+    public int getResult() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/DayThree//advent3.txt"));
+
+        List<Integer> validNumbers = new ArrayList<>();
+
+        String fileLine;
+        int iterator = 0;
+        while ((fileLine = reader.readLine()) != null) {
+            lines.add(iterator, fileLine);
+            iterator++;
         }
-        return sum;
+        reader.close();
+
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher;
+        for (int i = 0; i < lines.size(); i++) {
+            matcher = pattern.matcher(lines.get(i));
+            while (matcher.find()) {
+                if (checkNumberValidity(matcher.group(), i, matcher.start())) {
+                    validNumbers.add(Integer.valueOf(matcher.group()));
+                }
+            }
+        }
+
+        return validNumbers.stream().mapToInt(Integer::intValue).sum();
     }
 
-    protected void run() throws FileNotFoundException {
-        int sum = getResult();
-        System.out.println("day two solution: " + sum);
+    private static boolean checkNumberValidity(String number, int lineIndex, int matchIndex) {
+        String previousLine = lineIndex > 0 ? lines.get(lineIndex - 1) : null;
+        String currentLine = lines.get(lineIndex);
+        String nextLine = lineIndex < lines.size() - 1 ? lines.get(lineIndex + 1) : null;
+
+        int currIndex;
+        for (int i = 0; i < number.length(); i++) {
+            currIndex = matchIndex + i;
+            if (checkForSymbolOnLine(currentLine, currIndex)) return true;
+            if (previousLine != null) {
+                if (checkForSymbolOnLine(previousLine, currIndex)) return true;
+            }
+            if (nextLine != null) {
+                if (checkForSymbolOnLine(nextLine, currIndex)) return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean checkForSymbolOnLine(String line, int index) {
+        Pattern pattern = Pattern.compile("[^\\d.]");
+        Matcher matcher;
+        if (index > 0) {
+            matcher = pattern.matcher(String.valueOf(line.charAt(index - 1)));
+            if (matcher.find()) return true;
+        }
+        if (index < line.length() - 1) {
+            matcher = pattern.matcher(String.valueOf(line.charAt(index + 1)));
+            if (matcher.find()) return true;
+        }
+        matcher = pattern.matcher(String.valueOf(line.charAt(index)));
+        return matcher.find();
+    }
+    public void run() throws IOException {
+        int result = getResult();
+        System.out.println("Day three solution: " + result);
     }
 }
