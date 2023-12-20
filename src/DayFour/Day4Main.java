a2239d23bf60cfbf17916c7b82b9aeef8d67ceeb
Lukas <lkohlfuerst@eurofunk.com>
2023-12-20 07:58:51 +0100

    added day 4 part 2

--- src/DayFour/Day4Main.java
+++ src/DayFour/Day4Main.java
@@ -6,5 +6,7 @@ public class Day4Main {
     public void run() throws FileNotFoundException {
         Day4Part1 day4Part1 = new Day4Part1();
         day4Part1.run();
+        Day4Part2 day4Part2 = new Day4Part2();
+        day4Part2.run();
     }
 }
---
+++ src/DayFour/Day4Part2.java
@@ -0,0 +1,45 @@
+package DayFour;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day4Part2 {
+    Pattern numberPattern = Pattern.compile("\\d+");
+
+    @SuppressWarnings("DuplicatedCode")
+    protected int getResult() throws FileNotFoundException {
+        int sum = 0;
+        File input = new File("src\\DayFour\\advent4.txt");
+        Scanner scannerInput = new Scanner(input);
+        while (scannerInput.hasNextLine()) {
+            String data = scannerInput.nextLine();
+            int cardValue = getCardValue(data);
+            sum += cardValue;
+        }
+        return sum;
+    }
+
+    @SuppressWarnings("DuplicatedCode")
+    protected int getCardValue(String data) {
+        int value = 0;
+        String winningNumbersString = data.substring(data.indexOf(':') + 1, data.indexOf('|'));
+        String numbersPresentString = data.substring(data.indexOf('|') + 1) + " ";
+        Matcher winningNumberMatcher = numberPattern.matcher(winningNumbersString);
+        while (winningNumberMatcher.find()) {
+            Pattern winNumberPattern = Pattern.compile("\\D" + winningNumberMatcher.group() + "\\D");
+            Matcher winNumberMatcher = winNumberPattern.matcher(numbersPresentString);
+            if (winNumberMatcher.find()) {
+                value = (value < 1) ? 1 : value * 2;
+            }
+        }
+        return value;
+    }
+
+    public void run() throws FileNotFoundException {
+        int sum = getResult();
+        System.out.println("day four solution part 2: " + sum);
+    }
+}
